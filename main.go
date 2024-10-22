90a97be1fb364d08c18ba1ad4badd33b2963cd91
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-10-22 20:22:56 +0200

    get the audio stream from youtube video

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/hraban/opus"
 	"github.com/joho/godotenv"
+	"github.com/kkdai/youtube/v2"
 )
 
 func init() {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
 }
 
@@ -22,22 +25,61 @@ var (
 	err      error
 	commands = []*discordgo.ApplicationCommand{
 		{
-			Name: "basic-command",
-			// All commands and options must have a description
-			// Commands/options without description will fail the registration
-			// of the command.
-			Description: "Basic command",
+			Name:        "play",
+			Description: "Play a youtube video in your current voice channel",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "url",
+					Description: "YouTube URL",
+					Type:        discordgo.ApplicationCommandOptionString,
+					Required:    true,
+				},
+			},
 		},
 	}
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		"play": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			url := i.ApplicationCommandData().Options[0].StringValue()
+
+			voiceChannelID := ""
+
+			guild, err := s.State.Guild(i.GuildID)
+
+			if err != nil {
+				log.Fatal("couldn't retrieve guild from state object!")
+			}
+
+			for _, state := range guild.VoiceStates {
+				if state.UserID == i.Member.User.ID {
+					voiceChannelID = state.ChannelID
+					break
+				}
+			}
+
+			if voiceChannelID == "" {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You need to be in a voice channel to use this command.",
+					},
+				})
+				return
+			}
+
+			vc, err := s.ChannelVoiceJoin(i.GuildID, voiceChannelID, false, true)
+			if err != nil {
+				log.Fatal("Error joining voice channel:", err)
+			}
+
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: "Hey there! Congratulations, you just executed your first slash command",
+					Content: "Joining voice channel and playing: " + url,
 				},
 			})
+
+			streamYouTubeAudio(vc, url)
 		},
 	}
 )
@@ -45,7 +87,7 @@ var (
 func init() {
 	s, err = discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
 	if err != nil {
-		log.Fatalf("Invalid bot parameters: %v", err)
+		log.Fatal("Invalid bot parameters: %v", err)
 	}
 }
 
@@ -86,4 +128,36 @@ func main() {
 	<-stop
 
 	log.Println("Gracefully shutting down.")
+}
+
+func streamYouTubeAudio(vc *discordgo.VoiceConnection, url string) {
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(url)
+	if err != nil {
+		log.Fatal("Error retrieving YouTube video:", err)
+	}
+
+	// Find an audio-only format (based on MimeType)
+	var audioFormat *youtube.Format
+	for _, format := range video.Formats {
+		// Check for audio-only formats (example: audio/opus or audio/webm)
+		if format.MimeType == "audio/webm; codecs=\"opus\"" {
+			audioFormat = &format
+			break
+		}
+	}
+
+	if audioFormat == nil {
+		fmt.Println("No suitable audio format found")
+		return
+	}
+
+	stream, _, err := client.GetStream(video, audioFormat)
+	if err != nil {
+		fmt.Println("Error getting video stream:", err)
+		return
+	}
+
+	/* TODO: figure out how to play the stream */
 }
