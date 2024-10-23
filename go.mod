1db8c86fc3a88522aa8ec820842654f342b5a565
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-10-23 21:44:48 +0200

    buffer

--- go.mod
+++ go.mod
@@ -4,18 +4,23 @@ go 1.23.1
 
 require (
 	github.com/bwmarrin/discordgo v0.28.1
-	github.com/hraban/opus v0.0.0-20230925203106-0188a62cb302
 	github.com/joho/godotenv v1.5.1
 	github.com/kkdai/youtube/v2 v2.10.1
+	github.com/qrtc/opus-go v0.0.1
 )
 
 require (
+	github.com/Andreychik32/ytdl v1.0.4 // indirect
+	github.com/antchfx/jsonquery v1.1.4 // indirect
+	github.com/antchfx/xpath v1.1.7 // indirect
 	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/rs/zerolog v1.19.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
 
+	"github.com/Andreychik32/ytdl"
 	"github.com/bwmarrin/discordgo"
-	"github.com/hraban/opus"
 	"github.com/joho/godotenv"
-	"github.com/kkdai/youtube/v2"
 )
 
 func init() {
@@ -40,48 +40,8 @@ var (
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"play": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			url := i.ApplicationCommandData().Options[0].StringValue()
-
-			voiceChannelID := ""
-
-			guild, err := s.State.Guild(i.GuildID)
-
-			if err != nil {
-				log.Fatal("couldn't retrieve guild from state object!")
-			}
-
-			for _, state := range guild.VoiceStates {
-				if state.UserID == i.Member.User.ID {
-					voiceChannelID = state.ChannelID
-					break
-				}
-			}
-
-			if voiceChannelID == "" {
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: "You need to be in a voice channel to use this command.",
-					},
-				})
-				return
-			}
-
-			vc, err := s.ChannelVoiceJoin(i.GuildID, voiceChannelID, false, true)
-			if err != nil {
-				log.Fatal("Error joining voice channel:", err)
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "Joining voice channel and playing: " + url,
-				},
-			})
-
-			streamYouTubeAudio(vc, url)
-		},
-	}
+			//nothing
+		}}
 )
 
 func init() {
@@ -128,36 +88,4 @@ func main() {
 	<-stop
 
 	log.Println("Gracefully shutting down.")
-}
-
-func streamYouTubeAudio(vc *discordgo.VoiceConnection, url string) {
-	client := youtube.Client{}
-
-	video, err := client.GetVideo(url)
-	if err != nil {
-		log.Fatal("Error retrieving YouTube video:", err)
-	}
-
-	// Find an audio-only format (based on MimeType)
-	var audioFormat *youtube.Format
-	for _, format := range video.Formats {
-		// Check for audio-only formats (example: audio/opus or audio/webm)
-		if format.MimeType == "audio/webm; codecs=\"opus\"" {
-			audioFormat = &format
-			break
-		}
-	}
-
-	if audioFormat == nil {
-		fmt.Println("No suitable audio format found")
-		return
-	}
-
-	stream, _, err := client.GetStream(video, audioFormat)
-	if err != nil {
-		fmt.Println("Error getting video stream:", err)
-		return
-	}
-
-	/* TODO: figure out how to play the stream */
 }
